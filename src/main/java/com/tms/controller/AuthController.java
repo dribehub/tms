768d472b006442b476c659b2830e09572fcca310
dribe <bekizepp@gmail.com>
2022-09-28 12:05:54 +0200

    rm AuthService

--- src/main/java/com/tms/controller/AuthController.java
+++ src/main/java/com/tms/controller/AuthController.java
@@ -3,30 +3,26 @@ package com.tms.controller;
 import com.tms.dto.UserDto;
 import com.tms.dto.details.UserDetailsImpl;
 import com.tms.security.JwtResponse;
-import com.tms.service.AuthService;
 import com.tms.service.UserService;
 import com.tms.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("auth")
 @RestController
 public class AuthController {
 
-    private final AuthService authService; /* TODO: remove later */
-    private final UserService userService;
-
     private final AuthenticationManager authManager;
+    private final UserService userService;
     private final JwtUtils jwtUtils;
 
     @PostMapping("register")
