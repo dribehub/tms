cf82c1854e554dbe77cbf0e499978123381f391d
dribe <bekizepp@gmail.com>
2022-09-29 15:56:37 +0200

    final push

--- src/main/java/com/tms/enums/TripReasonEnum.java
+++ src/main/java/com/tms/enums/TripReasonEnum.java
@@ -1,5 +1,36 @@
 package com.tms.enums;
 
+import com.tms.entity.TripReason;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum TripReasonEnum {
-    MEETING, TRAINING, PROJECT, WORKSHOP, EVENT, OTHER
+
+    MEETING(1),
+    TRAINING(2),
+    PROJECT(3),
+    WORKSHOP(4),
+    EVENT(5),
+    OTHER(6);
+
+    public final TripReason tripReason;
+    public final Integer id;
+
+    TripReasonEnum(Integer id) {
+        this.tripReason = new TripReason(id, name());
+        this.id = id;
+    }
+
+    public static Optional<TripReason> getTripReasonByName(String name) {
+        return Arrays.stream(TripReasonEnum.values())
+                .filter(tre -> tre.name().equals(name.toUpperCase()))
+                .map(tre -> tre.tripReason)
+                .findFirst();
+    }
+
+    public static String[] names() {
+        return Arrays.stream(TripReasonEnum.values())
+                .map(TripReasonEnum::name).toArray(String[]::new);
+    }
 }
--- src/main/java/com/tms/enums/TripStatusEnum.java
+++ src/main/java/com/tms/enums/TripStatusEnum.java
@@ -1,5 +1,33 @@
 package com.tms.enums;
 
+import com.tms.entity.TripStatus;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum TripStatusEnum {
-    CREATED, WAITING_FOR_APPROVAL, APPROVED
+
+    CREATED(1),
+    WAITING_FOR_APPROVAL(2),
+    APPROVED(3);
+
+    public final TripStatus tripStatus;
+    public final Integer id;
+
+    TripStatusEnum(Integer id) {
+        this.tripStatus = new TripStatus(id, name());
+        this.id = id;
+    }
+
+    public static Optional<TripStatus> getTripStatusByName(String name) {
+        return Arrays.stream(TripStatusEnum.values())
+                .filter(re -> re.name().equals(name.toUpperCase()))
+                .map(re -> re.tripStatus)
+                .findFirst();
+    }
+
+    public static String[] names() {
+        return Arrays.stream(TripStatusEnum.values())
+                .map(TripStatusEnum::name).toArray(String[]::new);
+    }
 }
